45c20c8d4d9467b8bb1ffac448df4097cd372afb
Gumball Studios <gumballstudios@gmail.com>
2018-02-23 17:31:52 -0500

    Hints
    Added hint display when the game is starting

--- 2018-02/Scripts/NinjaLauncher.gd
+++ 2018-02/Scripts/NinjaLauncher.gd
@@ -12,6 +12,7 @@ var factory
 var soundBuffer = true
 var aiming = false
 var ready = false
+var hint_index = 1
 
 var factoryScene = preload("res://Objects/Ninjas/NinjaFactory.tscn")
 
@@ -19,6 +20,17 @@ var factoryScene = preload("res://Objects/Ninjas/NinjaFactory.tscn")
 func _ready():
 	factory = factoryScene.instance()
 	startPosition = $NinjaHideout.position
+	FadeHints(null, $Hud/Hint1)
+
+
+func FadeHints(outNode, inNode):
+	if outNode:
+		print("fade out: ", outNode)
+		$Hud/Fader.interpolate_property(outNode, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	if inNode:
+		print("fade in: ", inNode)
+		$Hud/Fader.interpolate_property(inNode, "modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1), 0.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	$Hud/Fader.start()
 
 
 func RecruitNinja(ninja_roster):
@@ -33,6 +45,10 @@ func _process(delta):
 	if !(ready && aiming):
 		return
 	
+	if hint_index == 4:
+		FadeHints($Hud/Hint3, null)
+		hint_index += 1
+	
 	var b = get_local_mouse_position()
 	$AimLine.position = b
 	$AimLine.show()
@@ -45,10 +61,16 @@ func _process(delta):
 func _input(event):
 	if event.is_action_pressed("fire"):
 		aiming = true
+		if hint_index == 1:
+			FadeHints($Hud/Hint1, $Hud/Hint2)
+			hint_index += 1
 	elif event.is_action_released("fire"):
 		if !(ready && aiming):
 			return
 		
+		if hint_index == 2:
+			FadeHints($Hud/Hint2, null)
+			hint_index += 1
 		ready = false
 		aiming = false
 		var a = startPosition
@@ -81,6 +103,9 @@ func _on_ninja_returned(ninja):
 	
 	ninjaReturned += 1
 	if ninjaReturned == $NinjaHideout.get_child_count():
+		if hint_index == 3:
+			FadeHints(null, $Hud/Hint3)
+			hint_index += 1
 		if recruitCount > 0:
 			for i in range(recruitCount):
 				var ninjaRecruit = factory.GetNinja()
