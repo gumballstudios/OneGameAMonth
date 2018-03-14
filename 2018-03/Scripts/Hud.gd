d85c98605f568892a8139742884d027213f02c8d
Gumball Studios <gumballstudios@gmail.com>
2018-03-14 10:18:24 -0400

    Frame and Hud Update
    Updated the sprite for the frame and buttons.  Updated the hud to use
    digital looking sprites for the score display

--- 2018-03/Scripts/Hud.gd
+++ 2018-03/Scripts/Hud.gd
@@ -4,6 +4,19 @@ extends Control
 var score = 0 setget SetScore, GetScore
 var miss = 0 setget SetMiss, GetMiss
 
+const digit_sprites = [ 
+	preload("res://Sprites/Digits/digit_0.png"),
+	preload("res://Sprites/Digits/digit_1.png"),
+	preload("res://Sprites/Digits/digit_2.png"),
+	preload("res://Sprites/Digits/digit_3.png"),
+	preload("res://Sprites/Digits/digit_4.png"),
+	preload("res://Sprites/Digits/digit_5.png"),
+	preload("res://Sprites/Digits/digit_6.png"),
+	preload("res://Sprites/Digits/digit_7.png"),
+	preload("res://Sprites/Digits/digit_8.png"),
+	preload("res://Sprites/Digits/digit_9.png")
+]
+
 
 func _ready():
 	pass
@@ -11,11 +24,29 @@ func _ready():
 
 func SetScore(value):
 	score = value
-	$Score.text = str(score)
+	var digits = GetDigits(score)
+	
+	$DigitContainer/HourDigits/Digit01.texture = digit_sprites[digits[0]]
+	$DigitContainer/HourDigits/Digit02.texture = digit_sprites[digits[1]]
+	$DigitContainer/MinuteDigits/Digit03.texture = digit_sprites[digits[2]]
+	$DigitContainer/MinuteDigits/Digit04.texture = digit_sprites[digits[3]]
 
 
 func GetScore():
 	return score
+
+
+func GetDigits(number):
+	var strNumber = str(number)
+	var digits = []
+	
+	for i in range(strNumber.length()):
+		digits.append(strNumber[i].to_int())
+	
+	while digits.size() < 4:
+		digits.insert(0, 0)
+	
+	return digits
 
 
 func SetMiss(value):
