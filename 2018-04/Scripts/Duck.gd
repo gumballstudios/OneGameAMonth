1fd6d0b7d35f3caa8a95f5fbf03e6479794e5f74
Gumball Studios <gumballstudios@gmail.com>
2018-04-16 20:47:50 -0400

    Duck animation
    Added shot animation for ducks

--- 2018-04/Scripts/Duck.gd
+++ 2018-04/Scripts/Duck.gd
@@ -3,6 +3,10 @@ extends PathFollow2D
 
 signal target_hit
 
+var targetSize = 20
+var points = 10
+var shot = false
+
 var speed = 250
 var direction = 1
 
@@ -15,22 +19,47 @@ func _ready():
 	
 	unit_offset = index
 	direction = direction - 2 * index
+	
+	#set_process(false)
 	pass
 
 
 func _process(delta):
-	offset += speed * delta * direction
+	offset += (speed + rand_range(-200, 100)) * delta * direction
+	#speed * delta * direction
+	#(speed + rand_range(-100, 100)) * delta * direction
 	if unit_offset < 0 or unit_offset > 1:
 		queue_free()
 
 
 func _on_shot(facing):
-	print("hit ", facing)
+	if shot:
+		return
+	
+	shot = true
 	var body = $Body.get_node(facing)
 	var pos = body.get_local_mouse_position() - body.get_node("Target").position
 	
-	if pos.length() <= 20:
+	if pos.length() < targetSize:
+		var percentage = (targetSize - pos.length()) / targetSize
+		var score = ceil(points * percentage)
+		print(points, " * ", percentage, " = ", score)
 		emit_signal("target_hit")
 	
-	queue_free()
+	set_process(false)
+	$DeathAnimation.interpolate_property(self, "rotation_degrees", rotation_degrees, rotation_degrees + rand_range(45, 90) * -direction, .75, Tween.TRANS_QUAD, Tween.EASE_IN)
+	$DeathAnimation.interpolate_method(self, "MoveDown", position.y, position.y + 400, .5, Tween.TRANS_BACK, Tween.EASE_IN)
+	$DeathAnimation.interpolate_method(self, "MoveSide", position.x, position.x + 100 * direction, .75, Tween.TRANS_QUART, Tween.EASE_OUT)
+	$DeathAnimation.start()
 
+
+func MoveDown(new_y):
+	position.y = new_y
+
+
+func MoveSide(new_x):
+	position.x = new_x
+
+
+func _on_death_completed( object, key ):
+	queue_free()
